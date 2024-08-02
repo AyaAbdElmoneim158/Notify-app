2711342c751d23e5cabcedf5bb534b43be715e7d
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2024-08-02 13:15:27 +0300

    Add Util

---
+++ app/lib/util/routes.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'globals.dart';
+
+class AppRoute {
+  static const String onBoard = '/';
+  static const String form = '/form';
+  static const String schedule = '/schedule';
+  static const String allTasks = '/allTasks';
+  static const String completedTasks = '/completedTasks';
+  static const String unCompletedTasks = '/unCompletedTasks';
+  static const String favoriteTasks = '/favoriteTasks';
+
+  static Route<dynamic> onGenerateRoutes(RouteSettings routeSettings) {
+    Widget getPage(String? name) {
+      switch (name) {
+        default:
+          return const BoardingScreen();
+      }
+    }
+
+    return CupertinoPageRoute(
+        settings: routeSettings,
+        builder: (context) => getPage(routeSettings.name));
+  }
+
+  static Route<T> getRoute<T>(Widget widget, [String? name]) {
+    return CupertinoPageRoute(
+        settings: RouteSettings(name: name), builder: (context) => widget);
+  }
+
+  static Future<T?>? push<T extends Object?>(Widget page, {String? name}) =>
+      Globals.instance.navigatorKey.currentState?.push<T>(getRoute(page, name));
+
+  static Future<T?>? pushReplacement<T extends Object?, TO extends Object>(
+          Widget page,
+          {String? name,
+          TO? result}) =>
+      Globals.instance.navigatorKey.currentState
+          ?.pushReplacement<T, TO>(getRoute(page, name), result: result);
+
+  static Future<T?>? pushNamed<T extends Object?>(String page) =>
+      Globals.instance.navigatorKey.currentState?.pushNamed<T>(page);
+
+  static void pop<T extends Object?>() =>
+      Globals.instance.navigatorKey.currentState?.pop<T>();
+}
+
+class BoardingScreen extends StatelessWidget {
+  const BoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("On the boarding screen")),
+    );
+  }
+}
