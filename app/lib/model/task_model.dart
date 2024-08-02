8e329c5d87e55b06953d1ce6cc4f517c9ce1a349
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2024-08-02 13:10:42 +0300

    Add task model + services

---
+++ app/lib/model/task_model.dart
@@ -0,0 +1,35 @@
+import '../styles/strings.dart';
+
+class TaskModel {
+  final String? title;
+  final String? start;
+  final String? end;
+  final String? reminder;
+  final String? repeat;
+  final String? date;
+  final String? status;
+  final String? favorite;
+
+  TaskModel(
+      {this.title,
+      this.start,
+      this.end,
+      this.reminder,
+      this.repeat,
+      this.date,
+      this.status,
+      this.favorite});
+
+  Map<String, dynamic> toMap() {
+    return {
+      columnTitle: title ?? "",
+      columnStart: start ?? "",
+      columnEnd: end ?? "",
+      columnReminder: reminder ?? "",
+      columnRepeat: repeat ?? "",
+      columnStatus: status ?? "",
+      columnDate: date ?? "",
+      columnFavorite: favorite ?? "",
+    };
+  }
+}
